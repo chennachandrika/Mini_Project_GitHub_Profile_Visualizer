fcebeea69877d51f3efa0d020d24799bc0ea5f74
chennachandrika <84555809+chennachandrika@users.noreply.github.com>
2021-08-12 13:13:52 +0530

    Common Components redirection

--- src/Components/common/FailureView/index.js
+++ src/Components/common/FailureView/index.js
@@ -4,13 +4,21 @@ import {
   FailureText,
   TryAgainButton
 } from "./styledComponents";
-import SomethingWentWrong from "../../LoginPage/resources/SomethingWentWrong.png";
-const FailureView = () => (
-  <FailureViewContainer>
-    <FailureViewLogo src={SomethingWentWrong} alt="something went wrong" />
-    <FailureText>Something went wrong. Please try again</FailureText>
-    <TryAgainButton>Try Again</TryAgainButton>
-  </FailureViewContainer>
-);
+
+const FailureView = () => {
+  const tryAgain = () => {
+    window.location.reload();
+  };
+  return (
+    <FailureViewContainer>
+      <FailureViewLogo
+        src="https://rawcdn.githack.com/chennachandrika/Mini_Project_Task_Flow_Manager/aff08ba65374b593c22bf3b26e9894ec8f812112/src/Components/LoginPage/resources/SomthingWentWrong.png"
+        alt="something went wrong"
+      />
+      <FailureText>Something went wrong. Please try again</FailureText>
+      <TryAgainButton onClick={tryAgain}>Try Again</TryAgainButton>
+    </FailureViewContainer>
+  );
+};
 
 export default FailureView;
--- src/Components/common/NotFoundView/index.js
+++ src/Components/common/NotFoundView/index.js
@@ -1,4 +1,5 @@
 import NotFoundViewImg from "../../LoginPage/resources/NotFoundViewImg.png";
+import { withRouter } from "react-router-dom";
 import {
   NotFoundViewContainer,
   NotFoundViewLogo,
@@ -7,16 +8,22 @@ import {
   GoBackButton
 } from "./styledComponents";
 
-const NotFoundView = () => (
-  <NotFoundViewContainer>
-    <NotFoundViewLogo src={NotFoundViewImg} alt="not found" />
-    <Heading>PAGE NOT FOUND</Heading>
-    <Text>
-      we're sorry, the page you requested could not be found please go back to
-      home
-    </Text>
-    <GoBackButton>Go Back</GoBackButton>
-  </NotFoundViewContainer>
-);
+const NotFoundView = (props) => {
+  const gotoBackPage = () => {
+    const { history } = props;
+    history.goBack();
+  };
+  return (
+    <NotFoundViewContainer>
+      <NotFoundViewLogo src={NotFoundViewImg} alt="not found" />
+      <Heading>PAGE NOT FOUND</Heading>
+      <Text>
+        we're sorry, the page you requested could not be found please go back to
+        home
+      </Text>
+      <GoBackButton onClick={gotoBackPage}>Go Back</GoBackButton>
+    </NotFoundViewContainer>
+  );
+};
 
-export default NotFoundView;
+export default withRouter(NotFoundView);
